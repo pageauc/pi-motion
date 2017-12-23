b59a142359742279f104fc3a7a286caf4d791469
Claude Pageau <pageauc@gmail.com>
2017-12-23 14:30:32 -0600

    Merge branch 'master' of https://github.com/pageauc/pi-motion-grive
    # Conflicts:
    #	source/setup.sh

--- source/setup.sh
+++ source/setup.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
+<<<<<<< HEAD
+# $0 ver 1.2 written by Claude Pageau
+
+install_Dir="pi-motion"
+
+cd ~
+mkdir -p $install_Dir
+cd install_Dir
+
+installFiles=("Readme.md" "pimotion.py" "pimotion.sh" "pi-timolo.sh" \
+"makeMovie.sh" "mvleavelast.sh" "makedailymovie.sh" "h2mp4.sh" "sync.sh")
+
+for fname in "${installFiles[@]}" ; do
+    wget_output=$(wget -O $fname -q --show-progress https://raw.github.com/pageauc/pi-motion/master/source/$fname)
+    if [ $? -ne 0 ]; then
+        wget_output=$(wget -O $fname -q https://raw.github.com/pageauc/pi-motion/master/source/plugins/$fname)
+        if [ $? -ne 0 ]; then
+            echo "ERROR - $fname wget Download Failed. Possible Cause Internet Problem."
+        else
+            wget -O $fname https://raw.github.com/pageauc/pi-motion/master/source/$fname
+        fi
+    fi
+done
+
+echo "Download and Install and pi-motion dependencies. One Moment Please ......."
+sudo apt-get install -y python-picamera python3-picamera gpac python-imaging 
+
+echo "Installing rclone"
+curl -L https://raw.github.com/pageauc/rclone4pi/master/rclone-install.sh | bash
+
+chmod +x *py
+chmod +x *sh
+
+echo "
+                   INSTRUCTIONS
+                   
+Make sure pi-camera is connected and enabled in raspi-config
+
+To Run run command below in a logged in console session
+
+cd $install_Dir
+./pimotion.py
+
+"
+
+
+=======
 echo "    Downloading and Installing pimotion dependencies."
 echo "    One Moment Please ......."
 sudo apt-get install -y python-picamera  python-imaging mencoder dos2unix gpac python-pyexiv2
@@ -11,3 +58,4 @@ echo "    Run pimotion.py with command below to Test.  See Readme.md"
 echo "    for additional instructions"
 echo "sudo ./pimotion.py"
 echo "See Readme.md for setting up gdrive security token"
+>>>>>>> a88d4d9572d371960d2bf1308a2bcd6cfb909cef
