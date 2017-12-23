99ee509f3977c01a0a5e7be3f6fba54b3cbeb083
Claude Pageau <pageauc@gmail.com>
2017-12-23 14:46:02 -0600

    Test update

--- source/setup.sh
+++ source/setup.sh
@@ -22,14 +22,15 @@ for fname in "${installFiles[@]}" ; do
     fi
 done
 
-echo "Download and Install and pi-motion dependencies. One Moment Please ......."
-sudo apt-get install -y python-picamera python3-picamera gpac python-imaging 
-
 echo "Installing rclone"
 curl -L https://raw.github.com/pageauc/rclone4pi/master/rclone-install.sh | bash
 
 chmod +x *py
 chmod +x *sh
+
+echo "    Downloading and Installing pimotion dependencies."
+echo "    One Moment Please ......."
+sudo apt-get install -y python-picamera python3-picamera python-imaging mencoder dos2unix gpac python-pyexiv2
 
 echo "
                    INSTRUCTIONS
@@ -42,19 +43,3 @@ cd $install_Dir
 ./pimotion.py
 
 "
-
-
-=======
-echo "    Downloading and Installing pimotion dependencies."
-echo "    One Moment Please ......."
-sudo apt-get install -y python-picamera  python-imaging mencoder dos2unix gpac python-pyexiv2
-sudo chmod +x gdrive
-sudo cp gdrive /usr/local/bin
-echo "    Install complete."
-echo "    Edit the pimotion.py variables to suit your needs per comments"
-echo "nano pimotion.py"
-echo "    Run pimotion.py with command below to Test.  See Readme.md"
-echo "    for additional instructions"
-echo "sudo ./pimotion.py"
-echo "See Readme.md for setting up gdrive security token"
-
